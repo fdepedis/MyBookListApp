7ae5d17918b3b3d0514aee6b6ec23e9b4960f79e
fdepedis <flavio.depedis@gmail.com>
2017-09-15 14:41:28 +0200

    add book loader and utils

---
+++ app/src/main/java/it/flaviodepedis/mybooklistapp/BookLoader.java
@@ -0,0 +1,56 @@
+package it.flaviodepedis.mybooklistapp;
+
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+import java.util.List;
+
+/**
+ * Created by flavio.depedis on 12/05/2017.
+ */
+
+public class BookLoader extends AsyncTaskLoader<List<Book>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = BookLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link BookLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public BookLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+
+        Log.w(LOG_TAG, "Log - onStartLoading() method");
+
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<Book> loadInBackground() {
+
+        Log.i(LOG_TAG, "Log - loadInBackground() method");
+
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of meteos.
+        List<Book> books = QueryUtils.fetchBookData(mUrl);
+        return books;
+    }
+}
