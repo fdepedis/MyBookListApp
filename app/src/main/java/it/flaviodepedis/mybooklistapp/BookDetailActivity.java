f5ff37256bea76ca50e40dec1f016b62d4645805
fdepedis <flavio.depedis@gmail.com>
2017-09-18 22:16:19 +0200

    update layout for rating bar

--- app/src/main/java/it/flaviodepedis/mybooklistapp/BookDetailActivity.java
+++ app/src/main/java/it/flaviodepedis/mybooklistapp/BookDetailActivity.java
@@ -6,15 +6,24 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * Created by flavio.depedis on 18/09/2017.
+ */
 public class BookDetailActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_detail_title) TextView tvTitleBook;
     @BindView(R.id.tv_detail_authors) TextView tvAuthorsBook;
+    @BindView(R.id.img_detail_icon) ImageView imgIconBook;
+    @BindView(R.id.tv_detail_price) TextView tvPriceBook;
+    @BindView(R.id.rating_bar_detail) RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +32,30 @@ public class BookDetailActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        String imageIcon;
+
         /** Get Intent Extras */
         if (getIntent().getExtras() != null) {
 
             //Intent intent = getIntent();
             Book currentBook = (Book) getIntent().getSerializableExtra("currentBook");
 
+            //set title of current book
             tvTitleBook.setText(currentBook.getmTitle());
+            //set authors of current book
             tvAuthorsBook.setText(currentBook.getmAuthor());
+            //set image of current book
+            imageIcon = currentBook.getmThumbnail();
+            if (imageIcon != null && imageIcon.length() > 0) {
+                Picasso.with(getApplicationContext()).load(currentBook.getmThumbnail()).into(imgIconBook);
+            } else {
+                Picasso.with(getApplicationContext()).load(R.drawable.image_not_found).into(imgIconBook);
+            }
+            //set rating of current book
+            ratingBar.setRating((float) currentBook.getmAverageRating());
+            //set price of current book
+            tvPriceBook.setText(currentBook.getmAmount());
+            //set title of current book
 
         }
     }
